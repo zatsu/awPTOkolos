7e053ea124dc70c235e1e8314efa13bc317515e9
Zatsu <maksymbielec@gmail.com\>
2016-05-30 19:33:31 +0200

    część 7 zadania

--- MortgageCalculator/src/test/java/kolokwium/c/pl/edu/us/MortgageCalculatorTest.java
+++ MortgageCalculator/src/test/java/kolokwium/c/pl/edu/us/MortgageCalculatorTest.java
@@ -2,10 +2,41 @@ package kolokwium.c.pl.edu.us;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.util.Arrays;
+import java.util.Collection;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+
+@RunWith(Parameterized.class)
 public class MortgageCalculatorTest
 {
+	double sum;
+	double interest;
+	int periods;
+	
+	MortgageCalculatorTest(double sum, double interest, int periods){
+		
+		this.sum=sum;
+		this.interest=interest;
+		this.periods=periods;
+		
+		
+	}
+	
+	
+	@Parameters
+    public static Collection<Object> data() {
+        
+		
+		Object[] data = new Object[] { 100.2, 500.3, 85.0 };
+        return Arrays.asList(data);
+    }
+	
+	
 	@Test
 	public void numberOfInstallmentsTest() {
 		fail("Not yet implemented");
