f66a32d98c76f134ec349a038e19cbac76bdcb1f
Zatsu <maksymbielec@gmail.com\>
2016-05-30 19:17:24 +0200

    Zadanie 6 dodanie testów

--- MortgageCalculator/src/test/java/kolokwium/c/pl/edu/us/CurrencyTest.java
+++ MortgageCalculator/src/test/java/kolokwium/c/pl/edu/us/CurrencyTest.java
@@ -1,18 +1,30 @@
 package kolokwium.c.pl.edu.us;
 
 import static org.junit.Assert.*;
-
+import junit.framework.Assert;
+//import kolokwium.c.pl.edu.us.Currency;
 import org.junit.Test;
 
 public class CurrencyTest
 {
+
+	String waluta= "PLN";
+	double exchange= 2.0;
+
+	String waluta2= "USD";
+	
+
 	@Test
 	public void convertFromPlnTest() {
-		fail("Not yet implemented");
-	}
+
+		Currency pln = new Currency(waluta, exchange);
+		Assert.assertEquals(5.0, pln.convertFromPLN(10.0));	
+}
 	
 	@Test
 	public void convertToPlnTest() {
-		fail("Not yet implemented");
+		Currency usd = new Currency(waluta2, exchange);
+		Assert.assertEquals(10.0, usd.convertToPLN(5.0));
+		
 	}
 }
